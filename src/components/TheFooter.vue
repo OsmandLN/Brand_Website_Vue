c10ddab2498d1031976bf95bfc0e3e2f356e5504
OsmandLN <osmand8370@gmail.com>
2022-08-05 15:45:14 +0800

    Feat: adjusted the content of the components.

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,9 +1,9 @@
 <template>
   <footer>
-    <span class="data-source">
+    <!-- <span class="data-source">
       資料來源: Data.Tainan 臺南市政府資料開放平台
-    </span>
-    <span class="contributor-name">Osmand ‧ 2022</span>
+    </span> -->
+    <span class="contributor-name">All right reserved by Osmand ‧ 2022</span>
   </footer>
 </template>
 
@@ -20,37 +20,38 @@ footer {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background-image: linear-gradient(
-    to right,
-    #ffffe0,
-    #effed6,
-    #d9fed2,
-    #bdfed4,
-    #9bfddc,
-    #96f8cf,
-    #92f2c1,
-    #8fecb3,
-    #b4dd8c,
-    #d4cb73,
-    #eeb66d,
-    #ffa07a
-  );
-  box-shadow: 2px 0px 10px $gray-blue;
+  // background-image: linear-gradient(
+  //   to right,
+  //   #ffffe0,
+  //   #effed6,
+  //   #d9fed2,
+  //   #bdfed4,
+  //   #9bfddc,
+  //   #96f8cf,
+  //   #92f2c1,
+  //   #8fecb3,
+  //   #b4dd8c,
+  //   #d4cb73,
+  //   #eeb66d,
+  //   #ffa07a
+  // );
+  background-color: rgba($color: white, $alpha: 1);
+  box-shadow: 0 2px 10px $gray-blue;
+  z-index: 999;
 
-  .data-source {
-    margin: 0 15px;
-  }
+  // .data-source {
+  //   margin: 0 15px;
+  // }
 
   span {
     font-family: $project-font-TC;
     font-size: 12px;
+    text-shadow: 1.5px 2px 2px gray;
   }
 }
 
 @media screen and (min-width: 768px) {
   footer {
-    flex-direction: row;
-
     span {
       font-size: 14px;
     }
@@ -59,6 +60,7 @@ footer {
 
 @media screen and (min-width: 1024px) {
   footer {
+    flex-direction: row;
     span {
       font-size: 16px;
     }
--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,31 +1,47 @@
 <template>
   <header>
     <div class="logo">
-      <router-link to="/home" class="website-title">
-        <span>趣</span>
-        <span>‧</span>
-        <span>臺</span>
-        <span>南</span>
+      <router-link to="/" class="brand-name">
+        <i class="fa-solid fa-leaf"></i>
+        TechPro
       </router-link>
     </div>
     <label class="navbar-toggle-label" for="navbar-toggle">
-      <span class="hamburger-top"></span>
-      <span class="hamburger-middle"></span>
-      <span class="hamburger-bottom"></span>
+      <span
+        class="hamburger-top"
+        @click="onHamburgerChanged"
+        :class="{ rotateClockwise: topHamburgerRotated }"
+      ></span>
+      <span
+        class="hamburger-middle"
+        @click="onHamburgerChanged"
+        :class="{ disappeared: middleHamburgerDisappeared }"
+      ></span>
+      <span
+        class="hamburger-bottom"
+        @click="onHamburgerChanged"
+        :class="{ rotateAntiClockwise: bottomHamburgerRotated }"
+      ></span>
     </label>
     <input type="checkbox" class="navbar-toggle" id="navbar-toggle" />
     <nav class="navbar">
       <ul class="nav-list">
         <li class="nav-item">
-          <router-link class="nav-link" to="/about">關於本站</router-link>
+          <router-link class="nav-link" to="/">首頁</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/home">回到首頁</router-link>
+          <router-link class="nav-link" to="/about">關於我們</router-link>
         </li>
         <li class="nav-item">
-          <router-link class="nav-link" to="/my_favorite_list"
-            >我的收藏</router-link
+          <router-link class="nav-link" to="/services">服務項目</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/successful-cases"
+            >成功案例</router-link
           >
+        </li>
+        <li class="nav-item">
+          <router-link class="nav-link" to="/contact">聯絡我們</router-link>
         </li>
       </ul>
     </nav>
@@ -45,47 +61,61 @@ header {
   top: 0;
   display: flex;
   align-items: center;
-  background-image: linear-gradient(
-    to right,
-    #ffffe0,
-    #effed6,
-    #d9fed2,
-    #bdfed4,
-    #9bfddc,
-    #96f8cf,
-    #92f2c1,
-    #8fecb3,
-    #b4dd8c,
-    #d4cb73,
-    #eeb66d,
-    #ffa07a
-  );
-  opacity: 1;
+  // background-image: linear-gradient(
+  //   to right,
+  //   #ffffe0,
+  //   #effed6,
+  //   #d9fed2,
+  //   #bdfed4,
+  //   #9bfddc,
+  //   #96f8cf,
+  //   #92f2c1,
+  //   #8fecb3,
+  //   #b4dd8c,
+  //   #d4cb73,
+  //   #eeb66d,
+  //   #ffa07a
+  // );
+  background-color: rgba($color: white, $alpha: 0.5);
   box-shadow: 0 2px 10px $gray-blue;
   z-index: 999;
 }
 
 // hamburger
 .hamburger-middle {
-  width: 25px;
+  width: 26px;
   height: 3px;
   position: relative;
   left: 35px;
   display: inline-block;
   border-radius: 10px;
   background-color: $gray-blue;
-  margin: 7px 0;
+  margin: 8px 0;
 }
 
 .hamburger-top,
 .hamburger-bottom {
-  width: 25px;
+  width: 26px;
   height: 3px;
   position: relative;
   left: 35px;
   display: inline-block;
   border-radius: 10px;
   background-color: $gray-blue;
+}
+
+.rotateClockwise {
+  transform: rotate(45deg) translateY(15.5px);
+  transition: 0.3s ease-in-out;
+}
+
+.disappeared {
+  transform: translateX(-200px);
+}
+
+.rotateAntiClockwise {
+  transform: rotate(-45deg) translateY(-15.5px);
+  transition: 0.3s ease-in-out;
 }
 
 // navbar
@@ -96,25 +126,26 @@ nav {
   top: 100%; // navbar從header底部開始做展開延伸
   display: flex;
   justify-content: center;
-  background-image: linear-gradient(
-    to right,
-    #ffffe0,
-    #effed6,
-    #d9fed2,
-    #bdfed4,
-    #9bfddc,
-    #96f8cf,
-    #92f2c1,
-    #8fecb3,
-    #b4dd8c,
-    #d4cb73,
-    #eeb66d,
-    #ffa07a
-  );
+  // background-image: linear-gradient(
+  //   to right,
+  //   #ffffe0,
+  //   #effed6,
+  //   #d9fed2,
+  //   #bdfed4,
+  //   #9bfddc,
+  //   #96f8cf,
+  //   #92f2c1,
+  //   #8fecb3,
+  //   #b4dd8c,
+  //   #d4cb73,
+  //   #eeb66d,
+  //   #ffa07a
+  // );
+  background-color: rgba($color: white, $alpha: 0.9);
   transform: scale(1, 0);
   transform-origin: top;
   transition: transform 0.3s ease-out;
-  box-shadow: 0 5px 5px rgba(0, 0, 0, 0.5);
+  // box-shadow: 0 5px 5px rgba(0, 0, 0, 0.5);
 
   .nav-list {
     display: flex;
@@ -130,11 +161,6 @@ nav {
       color: $gray-blue;
       font-family: $project-font-TC;
       font-weight: normal;
-
-      // &:hover {
-      //   transform: scale(1.1);
-      //   transition: 0.2s;
-      // }
     }
   }
 }
@@ -156,8 +182,6 @@ nav {
 
 .navbar-toggle-label {
   height: 100%;
-  // position: absolute;
-  // top: 0;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -172,7 +196,7 @@ nav {
   justify-content: center;
   transform: translateX(-50%);
 
-  .website-title {
+  .brand-name {
     color: $gray-blue;
     font-family: $project-font-TC;
     font-size: 24px;
@@ -181,60 +205,15 @@ nav {
   }
 }
 
-@media screen and (min-width: 768px) {
-  // nav {
-  //   position: absolute;
-  //   left: 0;
-  // }
-  // .navbar-toggle {
-  //   &:checked {
-  //     ~ nav {
-  //       transform: scale(0.9, 1);
-  //     }
-  //   }
-  // }
-  // header {
-  //   max-width: 100%;
-  // }
-  // .logo {
-  //   position: relative;
-  //   left: 120px;
-  // }
-  // .navbar-toggle-label {
-  //   display: none;
-  // }
+@media screen and (min-width: 576px) {
+}
 
-  // nav {
-  //   all: unset;
-  //   width: 50%;
-  //   transform: translateX(50%);
-  //   align-items: center;
-  //   .nav-list {
-  //     width: 90%;
-  //     display: flex;
-  //     flex-direction: row;
-  //     .nav-item {
-  //       width: 40%;
-  //       display: flex;
-  //       align-items: center;
-  //     }
-  //   }
-  // }
+@media screen and (min-width: 768px) {
 }
 
 @media screen and (min-width: 1024px) {
-  // nav {
-  //   .nav-list {
-  //     .nav-link {
-  //       font-size: 16px;
-  //     }
-  //   }
-  // }
-}
-
-@media screen and (min-width: 1200px) {
   .logo {
-    left: 10%;
+    left: 15%;
   }
 
   .navbar-toggle-label {
@@ -244,6 +223,8 @@ nav {
   nav {
     all: unset;
     width: 70%;
+    position: relative;
+    left: 10%;
     margin: 0 auto;
 
     .nav-list {
@@ -251,6 +232,33 @@ nav {
       margin-bottom: 0;
       justify-content: space-evenly;
     }
+
+    .nav-item {
+      .nav-link {
+        &:hover {
+          text-shadow: 1.5px 2px 2px gray;
+        }
+      }
+    }
   }
 }
 </style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      topHamburgerRotated: false,
+      middleHamburgerDisappeared: false,
+      bottomHamburgerRotated: false
+    }
+  },
+  methods: {
+    onHamburgerChanged() {
+      this.topHamburgerRotated = !this.topHamburgerRotated
+      this.middleHamburgerDisappeared = !this.middleHamburgerDisappeared
+      this.bottomHamburgerRotated = !this.bottomHamburgerRotated
+    }
+  }
+}
+</script>
