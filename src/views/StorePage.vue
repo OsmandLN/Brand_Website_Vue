47c191ef3447ae80fa3269f723e126d418ac1e8c
OsmandLN <osmand8370@gmail.com>
2022-12-04 02:41:28 +0800

    feat: built store page

---
+++ src/views/StorePage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="products-wrapper">
+    <ProductCard v-for="product in getProducts" :key="product.id" :initial-product="product" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$project-font-TC: 'Noto Sans TC', sans-serif;
+
+.products-wrapper {
+  @include mainContentContainerSetting;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-family: $project-font-TC;
+}
+
+@media screen and (min-width:1024px) {
+  .products-wrapper {
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+  }
+}
+</style>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue'
+// import Products from '@/assets/datas/products.json'
+
+export default {
+  components: {
+    ProductCard
+  },
+  computed: {
+    getProducts() {
+      return this.$store.state.products
+    }
+  },
+  created() {
+    this.$store.dispatch('fetchProductsInfo')
+  }
+  // data() {
+  //   return {
+  //     products: Products.results
+  //   }
+  // }
+}
+</script>
